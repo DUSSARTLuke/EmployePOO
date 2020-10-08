415cfd4a469fdbb94c806d42027bb5932a8246d0
DUSSART Luke <lukedussart@hotmail.fr>
2020-10-08 10:19:44 +0200

    rajout class employe et traitement

---
+++ classes/Employe.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Employe
+ *
+ * @author dussart.luke
+ */
+class Employe
+{
+    protected int $numero;
+    protected string $nom;
+    protected string $prenom;
+    protected DateTime $dateDeNaissance;
+    protected float $salaireM;
+    
+    public function __construct(int $pnum, string $pnom, string $pprenom, DateTime $pddn, float $psalaire)
+    {
+        $this->numero = $pnum;
+        $this->nom = $pnom;
+        $this->prenom = $pprenom;
+        $this->dateDeNaissance = $pddn;
+        $this->salaireM = $psalaire;
+    }
+    public function getNumero(): int
+    {
+        return $this->numero;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    public function getDateDeNaissance(): DateTime
+    {
+        return $this->dateDeNaissance;
+    }
+
+    function getSalaireM(): float
+    {
+        return $this->salaireM;
+    }
+
+    function setSalaireM(float $salaireM): void
+    {
+        $this->salaireM = $salaireM;
+    }
+
+
+}
